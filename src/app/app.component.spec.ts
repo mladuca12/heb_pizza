e86c8ce73ac9e0d6a52939af884444fa2a47bef6
Matt LaDuca <foobar402@gmail.com>
2023-05-27 13:13:09 -0500

    initial commit.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,11 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('heb-pizza');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('heb-pizza app is running!');
-  });
 });
