292b87c2f96947094b9b3fa33a7aead6dd028a91
Matt LaDuca <foobar402@gmail.com>
2023-05-27 18:49:44 -0500

    initial submit order call.

--- src/app/components/order/order.component.ts
+++ src/app/components/order/order.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Order, OrderService } from 'src/app/services/order.service';
 
 @Component({
   selector: 'app-order',
@@ -11,17 +12,33 @@ export class OrderComponent {
   isLoading = false;
   isOrderCreationFailed = false;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private orderService: OrderService
+  ) {
     this.orderForm = this.formBuilder.group({
       crust: ['', Validators.required],
       flavor: ['', Validators.required],
       size: ['', Validators.required],
-      tableNumber: [null, Validators.required],
+      tableNumber: ['', Validators.required],
     });
   }
 
   onSubmit() {
-   if (!this.orderForm.valid) return;
-   console.log('submit order');
+    if (!this.orderForm.valid) return;
+    this.isLoading = true;
+    this.orderService
+      .createOrder(
+        new Order(
+          this.orderForm.value.crust,
+          this.orderForm.value.flavor,
+          this.orderForm.value.size,
+          Number(this.orderForm.value.tableNumber)
+        )
+      )
+      .subscribe({
+        next: () => console.log('success'),
+        error: (e) => console.log(e),
+      });
   }
 }
