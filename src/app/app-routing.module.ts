9be306c48abe6fa765663589519e221c7c20a241
Matt LaDuca <foobar402@gmail.com>
2023-05-27 13:26:25 -0500

    initial components.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { OrderComponent } from './components/order/order.component';
+import { OrdersComponent } from './components/orders/orders.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+   { path: 'orders', component: OrdersComponent },
+   { path: 'order', component: OrderComponent },
+   { path: 'login', component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { OrdersComponent } from './components/orders/orders.component';
+import { OrderComponent } from './components/order/order.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    OrdersComponent,
+    OrderComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
