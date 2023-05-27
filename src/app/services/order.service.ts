3f741c93d7972c59bca1ec6c67dc596fae308d1f
Matt LaDuca <foobar402@gmail.com>
2023-05-27 18:22:03 -0500

    initial implementation of order service.

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -1,9 +1,57 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
+
+export class Order {
+  constructor(
+    public crust: string,
+    public flavor: string,
+    public size: string,
+    public tableNumber: number,
+    public id?: number,
+    public timeStamp?: string
+  ) {}
+}
+
+interface OrderResponse {
+  Order_ID: number;
+  Crust: string;
+  Flavor: string;
+  Size: string;
+  Table_No: number;
+  Timestamp: string;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class OrderService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  createOrder(order: Order): Observable<Order> {
+    return this.http
+      .post<OrderResponse>('https://pizza-api-app.herokuapp.com/api/orders', {
+        Crust: order.crust,
+        Flavor: order.flavor,
+        Size: order.size,
+        Table_No: order.tableNumber,
+      })
+      .pipe(
+        map(
+          (response) =>
+            new Order(
+              response.Crust,
+              response.Flavor,
+              response.Size,
+              response.Table_No,
+              response.Order_ID,
+              response.Timestamp
+            )
+        )
+      );
+  }
+
+  getOrders() {}
+
+  deleteOrder(id: number) {}
 }
