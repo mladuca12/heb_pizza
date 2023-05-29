c697d86bf8f93f0db9bc4d867980411ac60c8e18
Matt LaDuca <foobar402@gmail.com>
2023-05-29 15:52:04 -0500

    add css for logo.

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -4,7 +4,8 @@ import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-nav',
-  templateUrl: './nav.component.html'
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent {
   isLoggedIn = false;
